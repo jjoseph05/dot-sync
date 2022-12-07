7e27a14c9b0f5e3a0f280a0988bba8f2fab0806d
Justin Joseph <justinjoseph1@gmail.com>
2022-12-08 02:25:03 +0900

    Adds env to gitignore, Extracts config values to env and data fetching logic to App, also Adds listen db call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import './App.css';
-import { getTestCoords } from './utils/firebase'
+import firebase from './utils/firebase';
+import {useEffect, useState} from 'react';
+import { getDatabase, ref, child, get, onValue } from "firebase/database";
+
 
 function App() {
+  const [coordinateslist, setCoordinateslist] = useState();
+  const [latestPosition, setLatestPosition] = useState();
+  const database = getDatabase(firebase);
+
+  useEffect(() =>{
+    const coordinateRef = ref(database);
+    let coordData = [];
+    get(child(coordinateRef, `users/john`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        // console.log(snapshot.val());
+        coordData.push(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+    setCoordinateslist(coordData);
+  }, [database])
+
+  useEffect(() =>{
+    // let coordSyncData = [];
+    const coordinates = ref(database, `users/john`);
+    onValue(coordinates, (snapshot) => {
+      const data = snapshot.val();
+      setLatestPosition(data);
+      // console.log('strar syc7',data);
+    });
+
+    // setCoordinateslist(coordData);
+  }, [database])
+
   console.log('this is the first test');
-  const coords = getTestCoords();
-  console.log('cords', coords);
+  console.log('cords', coordinateslist);
+  console.log('pozition-realtime', latestPosition);
+
+  //  let {x, y} = coordinateslist;
+  // console.log('x', x);
+  // console.log('y', y);
 
   return (
     <div className="App">
       <h2>Hello, dot-sync</h2>
+      {coordinateslist
+        ? coordinateslist.map((coordinate) => {console.log('okkk',coordinate)})
+        : <p>fuccck</p>}
     </div>
   );
 }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,40 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, child, get } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAj95TOCopic-es3O9XV0zM95g9moNL_qI",
-  authDomain: "dot-sync-82838.firebaseapp.com",
-  databaseURL: "https://dot-sync-82838-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "dot-sync-82838",
-  storageBucket: "dot-sync-82838.appspot.com",
-  messagingSenderId: "556254892037",
-  appId: "1:556254892037:web:e5d3f77b0fe1d00b044c6c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_APP_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-const dbRef = ref(database);
-const getTestCoords = () => {
-  const coordData = [];
-  get(child(dbRef, `users/john`)).then((snapshot) => {
-    if (snapshot.exists()) {
-      console.log(snapshot.val());
-      coordData.push(snapshot.val());
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-  return coordData;
-}
+const firebase = initializeApp(firebaseConfig);
 
 
-export { getTestCoords };
+
+export default firebase;
