010a53d32f22d737157229eba5ce9acc781a6c6e
Justin Joseph <justinjoseph1@gmail.com>
2022-12-09 02:05:00 +0900

    Tracks mouse movent, debugging pushing state data, need to refactor mousehandler fn; it does too much, also state is in conditional, that is likly part of the cause

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import firebase from './utils/firebase';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 import { getDatabase, ref, child, get, onValue, set } from "firebase/database";
 
 function App() {
@@ -8,7 +8,9 @@ function App() {
   const [latestPosition, setLatestPosition] = useState();
   const [movedToXy, setMovedToXy] = useState({x:0, y:1});
   const database = getDatabase(firebase);
-
+  // TODO: turn this into a ref
+  const dotElementRef = useRef(null);
+  let dragValue;
 
   //Get - read data once
   useEffect(() =>{
@@ -53,30 +55,55 @@ function App() {
     // }
   }, [movedToXy])
 
+  useEffect(() =>{
+  
+  }, [])
+
   const handleClick = (e) => {
     console.log("I clicked ", e);
     setMovedToXy({ x:e.pageX, y:e.pageY});
   }
 
+  const handleMouseEvent = (e) => {
+
+    if (e.type === 'mousedown') {
+      dragValue = dotElementRef;
+    }
+
+    if (e.type === 'mouseup') {
+      dragValue = null;
+    }
+    if (e.type === 'mousemove' && (dragValue !== undefined && dragValue !== null )) {
+      debugger;
+      let x = e.pageX;
+      console.log(x);
+      let y = e.pageY;
+      console.log(y);
+      setMovedToXy(x,y);
+      console.log(dragValue);
+      debugger;
+
+      dragValue.current.style.left = (x-5) + "px";
+      dragValue.current.style.top = (y-5) + "px";
+    }
+  }
   console.log('this is the first test');
   console.log('cords', coordinateslist);
   console.log('pozition-realtime', latestPosition);
 
-  //  let {x, y} = coordinateslist;
-  // console.log('x', x);
-  // console.log('y', y);
-
   return (
-    <div onClick={handleClick} className="App" style={{ backgroundColor: 'brown', width: '100%', height: '500px'}}>
+    <div onMouseMove={handleMouseEvent} onMouseDown={handleMouseEvent} onMouseUp={handleMouseEvent} className="App" style={{ backgroundColor: 'brown', width: '100%', height: '500px'}}>
       <h2>Hello, dot-sync</h2>
       <div
         id="theDot"
+        ref={dotElementRef}
         style={{
           width: '10px',
           height: '10px',
           borderRadius: '50%',
           border: '2px solid black',
-          backgroundColor: 'white'
+          backgroundColor: 'white',
+          position: "absolute"
         }}>
       </div>
       {latestPosition
