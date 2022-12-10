9efc7e62e6c9ad076f6c0f5614e96134e8f839c5
Justin Joseph <justinjoseph1@gmail.com>
2022-12-11 04:38:07 +0900

    Corrects movement of dot, Adds sync movement to remote client dot

--- src/App.js
+++ src/App.js
@@ -1,74 +1,108 @@
 import './App.css';
 import firebase from './utils/firebase';
+import DragMove from "./DragMove";
 import {useEffect, useState, useRef} from 'react';
 import { getDatabase, ref, onValue, set } from "firebase/database";
 
 function App() {
-  const [latestPosition, setLatestPosition] = useState();
-  const [movedToXy, setMovedToXy] = useState({x:0, y:1});
+  const [remoteTranslate, setRemoteTranslate] = useState({x:0, y:1});
+  const [translate, setTranslate] = useState({x:0, y:1});
+  const [isDragging, setIsDragging] = useState(false);
   const database = getDatabase(firebase);
-  const dotElementRef = useRef(null);
+  const dotRef = useRef(null);
   let dragValue;
 
   //onvalue listend for event
   useEffect(() =>{
-    const coordinates = ref(database, `users/john`);
-    onValue(coordinates, (snapshot) => {
+    const position = ref(database, `users/john`);
+    onValue(position, (snapshot) => {
       let data = snapshot.val();
-
-      setLatestPosition(data);
+        setRemoteTranslate({ x: data.x, y: data.y})
     });
-    console.log('listen event fires');
+
   }, [database])
 
   //set, write to db
   useEffect(() =>{
       const db = getDatabase();
       set(ref(db, 'users/john'), {
-        x: movedToXy.x,
-        y: movedToXy.y
+        x: translate.x,
+        y: translate.y,
+        driver: false
       });
-  }, [movedToXy])
+  }, [translate])
 
-  const handleMouseDown = (e) => {
-    dragValue = dotElementRef;
+  useEffect(() => {
+    const element = dotRef.current;
+    if (element) {
+      // element.addEventListener('mousedown', handlePointerDown);
+      window.addEventListener('mouseup', handlePointerUp);
+      // element.addEventListener('mousemove', handlePointerMove);
+
+      return () => {
+        element.removeEventListener('mousedown', handlePointerDown);
+        window.removeEventListener('mouseup', handlePointerUp);
+        element.removeEventListener('mousemove', handlePointerMove);
+      };
+    }
+
+    return () => {};
+  }, [translate, isDragging]);
+
+  const handleDragMove = (e) => {
+    console.log(e);
+    // debugger;
+    setTranslate({
+      x: e.pageX,
+      y: e.pageY
+    });
+  };
+
+  const handlePointerDown = (e) => {
+    setIsDragging(true);
+    dragValue = dotRef;
   }
-  const handleMouseUp = (e) => {
+  const handlePointerUp = (e) => {
+    setIsDragging(false);
     dragValue = null;
   }
 
-  const handleMouseMove = (e) => {
-      let x = e.pageX;
-      let y = e.pageY;
-      // if(dragValue !== null && dragValue !== undefined ) {
-      //   dragValue.current.style.left = (x-5) + "px";
-      //   dragValue.current.style.top = (y-5) + "px";
-      // }
-      setMovedToXy({x,y});
+  const handlePointerMove = (e) => {
+    if (isDragging) {
+      handleDragMove(e);
+
+      // let x = e.pageX;
+      // let y = e.pageY;
+      //     dotRef.current.style.left = (x-5) + "px";
+      //     dotRef.current.style.top = (y-5) + "px";
+      }
   }
+  let postion = isDragging ? translate : remoteTranslate;
   return (
     <div
-      onMouseMove={handleMouseMove}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
+      onMouseMove={handlePointerMove}
+      onMouseDown={handlePointerDown}
+      // onMouseUp={handleMouseUp}
       className="App"
       style={{ backgroundColor: 'brown', width: '100%', height: '500px'}}
     >
       <h2>Hello, dot-sync</h2>
-      <div
-        id="theDot"
-        ref={dotElementRef}
-        style={{
-          width: '30px',
-          height: '30px',
-          borderRadius: '50%',
-          border: '2px solid black',
-          backgroundColor: 'white',
-          position: "absolute"
-        }}>
-      </div>
-      {latestPosition
-        ? <p>x {latestPosition.x}, y {latestPosition.y}</p>
+        <div
+          ref={dotRef}
+          style={{
+            backgroundColor: 'white',
+            border: '2px solid black',
+            borderRadius: '50%',
+            height: '10px',
+            // transform: `translateX(${translate.x}px) translateY(${translate.y}px)`,
+            position: 'absolute',
+            top: `${postion.y - 5}px`,
+            left: `${postion.x - 5}px`,
+            width: '10px'
+          }}>
+        </div>
+      {postion
+        ? <p>x: {postion.x}, y {postion.y }, driverStatus: {!!postion.driver ? 'you be driving' : 'sitting shotgun'}</p>
         : <p>oops, something broke</p>}
     </div>
   );
