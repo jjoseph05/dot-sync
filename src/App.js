0743e9342c7b638191975aa39d62029d0b7a68e9
Justin Joseph <justinjoseph1@gmail.com>
2022-12-10 17:16:58 +0900

    Removes get(read once from db), testing handlers, extra consoles, Corrects setState call for setting position: data is pushing ok now

--- src/App.js
+++ src/App.js
@@ -1,105 +1,66 @@
 import './App.css';
 import firebase from './utils/firebase';
 import {useEffect, useState, useRef} from 'react';
-import { getDatabase, ref, child, get, onValue, set } from "firebase/database";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 
 function App() {
-  const [coordinateslist, setCoordinateslist] = useState();
   const [latestPosition, setLatestPosition] = useState();
   const [movedToXy, setMovedToXy] = useState({x:0, y:1});
   const database = getDatabase(firebase);
-  // TODO: turn this into a ref
   const dotElementRef = useRef(null);
   let dragValue;
 
-  //Get - read data once
+  //onvalue listend for event
   useEffect(() =>{
-    const coordinateRef = ref(database);
-    let coordData = [];
-    get(child(coordinateRef, `users/john`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        // console.log(snapshot.val());
-        coordData.push(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-    setCoordinateslist(coordData);
-  }, [database])
-
-  //onvalue listend for events
-  useEffect(() =>{
-    // let coordSyncData = [];
     const coordinates = ref(database, `users/john`);
     onValue(coordinates, (snapshot) => {
-      const data = snapshot.val();
-      console.log("sycho", data);
-      setLatestPosition(data);
-      // console.log('strar syc7',data);
-    });
+      let data = snapshot.val();
 
-    // setCoordinateslist(coordData);
+      setLatestPosition(data);
+    });
+    console.log('listen event fires');
   }, [database])
 
+  //set, write to db
   useEffect(() =>{
-    // function writeUserData(userId, name, email, imageUrl) {
-      console.log('it was me. the post pusher.');
-      console.log(movedToXy);
       const db = getDatabase();
       set(ref(db, 'users/john'), {
         x: movedToXy.x,
         y: movedToXy.y
       });
-    // }
   }, [movedToXy])
 
-  useEffect(() =>{
-  
-  }, [])
-
-  const handleClick = (e) => {
-    console.log("I clicked ", e);
-    setMovedToXy({ x:e.pageX, y:e.pageY});
+  const handleMouseDown = (e) => {
+    dragValue = dotElementRef;
+  }
+  const handleMouseUp = (e) => {
+    dragValue = null;
   }
 
-  const handleMouseEvent = (e) => {
-
-    if (e.type === 'mousedown') {
-      dragValue = dotElementRef;
-    }
-
-    if (e.type === 'mouseup') {
-      dragValue = null;
-    }
-    if (e.type === 'mousemove' && (dragValue !== undefined && dragValue !== null )) {
-      debugger;
+  const handleMouseMove = (e) => {
       let x = e.pageX;
-      console.log(x);
       let y = e.pageY;
-      console.log(y);
-      setMovedToXy(x,y);
-      console.log(dragValue);
-      debugger;
-
-      dragValue.current.style.left = (x-5) + "px";
-      dragValue.current.style.top = (y-5) + "px";
-    }
+      // if(dragValue !== null && dragValue !== undefined ) {
+      //   dragValue.current.style.left = (x-5) + "px";
+      //   dragValue.current.style.top = (y-5) + "px";
+      // }
+      setMovedToXy({x,y});
   }
-  console.log('this is the first test');
-  console.log('cords', coordinateslist);
-  console.log('pozition-realtime', latestPosition);
-
   return (
-    <div onMouseMove={handleMouseEvent} onMouseDown={handleMouseEvent} onMouseUp={handleMouseEvent} className="App" style={{ backgroundColor: 'brown', width: '100%', height: '500px'}}>
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      className="App"
+      style={{ backgroundColor: 'brown', width: '100%', height: '500px'}}
+    >
       <h2>Hello, dot-sync</h2>
       <div
         id="theDot"
         ref={dotElementRef}
         style={{
-          width: '10px',
-          height: '10px',
+          width: '30px',
+          height: '30px',
           borderRadius: '50%',
           border: '2px solid black',
           backgroundColor: 'white',
