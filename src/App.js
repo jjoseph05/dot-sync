dd95f8d228c99d0fac0330b2be629b809d6b341d
Justin Joseph <justinjoseph1@gmail.com>
2022-12-12 21:57:19 +0900

    Renames vars: translate to dotPosition, Switches use of pageX/pageY to movementX/movementY(fixes slight jump when dragging dot), Switches use of adjusting dot top/left position to transform property(avoids layout and paint), Removes unused ref, and db property

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import './App.css';
 import firebase from './utils/firebase';
-import {useEffect, useState, useRef} from 'react';
+import { useEffect, useState } from 'react';
 import { getDatabase, ref, onValue, set } from "firebase/database";
 
 function App() {
-  //TODO: document.width / 2 , document.height / 2] NG! needs to be the same on different szied clients, use something based on 0,0
-  //TODO: Rename translate &6 remoteTranslate, maybe dotPosition
-  //TODO: 
-  const defaultDotPosition = { x: 0, y: 0 }
-  const [remoteTranslate, setRemoteTranslate] = useState(defaultDotPosition);
-  const [translate, setTranslate] = useState(defaultDotPosition);
+  const defaultDotPosition = { x: 300, y: 150 }
+  const [remoteDotPosition, setRemoteDotPosition] = useState(defaultDotPosition);
+  const [dotPosition, setDotPosition] = useState(defaultDotPosition);
   const [isDragging, setIsDragging] = useState(false);
   const database = getDatabase(firebase);
-  const dotRef = useRef(null);
 
-  //onvalue listend for event
+  //onvalue, listend for event
   useEffect(() =>{
-    const position = ref(database, `users/john`);
-    onValue(position, (snapshot) => {
-      let data = snapshot.val();
-        setRemoteTranslate({ x: data.x, y: data.y})
+    const positionDB = ref(database, `users/john`);
+    onValue(positionDB, (snapshot) => {
+      let dbDotPosition = snapshot.val();
+      setRemoteDotPosition({ x: dbDotPosition.x, y: dbDotPosition.y })
     });
 
   }, [database])
 
   //set, write to db
   useEffect(() =>{
-      const db = getDatabase();
-      set(ref(db, 'users/john'), {
-        x: translate.x,
-        y: translate.y,
-        driver: false
-      });
-  }, [translate])
+    const db = getDatabase();
+    set(ref(db, 'users/john'), {
+      x: dotPosition.x,
+      y: dotPosition.y
+    });
+
+  }, [dotPosition])
 
   useEffect(() => {
     window.addEventListener('pointerup', handlePointerUp);
@@ -43,9 +39,9 @@ function App() {
   }, [isDragging]);
 
   const handleDragMove = (e) => {
-    setTranslate({
-      x: e.pageX,
-      y: e.pageY
+    setDotPosition({
+      x: dotPosition.x + e.movementX,
+      y: dotPosition.y + e.movementY,
     });
   };
 
@@ -53,6 +49,7 @@ function App() {
     console.log(e);
     setIsDragging(true);
   }
+
   const handlePointerUp = (e) => {
     setIsDragging(false);
   }
@@ -62,7 +59,10 @@ function App() {
       handleDragMove(e);
     }
   }
-  let postion = isDragging ? translate : remoteTranslate;
+
+  let position = isDragging ? dotPosition : remoteDotPosition;
+  let dotSize = 20;
+
   return (
     <div
       onPointerMove={handlePointerMove}
@@ -71,22 +71,19 @@ function App() {
     >
       <h2>Hello, dot-sync</h2>
         <div
-          ref={dotRef}
           onPointerDown={handlePointerDown}
           style={{
             backgroundColor: 'white',
             border: '2px solid black',
             borderRadius: '50%',
             cursor: 'pointer',
-            height: '10px',
-            position: 'absolute',
-            top: `${postion.y - 5}px`,
-            left: `${postion.x - 5}px`,
-            width: '10px'
+            height: `${dotSize}px`,
+            transform: `translateX(${position.x}px) translateY(${position.y}px) translateZ(0)`,
+            width: `${dotSize}px`
           }}>
         </div>
-      {postion
-        ? <p>x: {postion.x}, y: {postion.y }</p>
+      {position
+        ? <p>x: {position.x}, y: {position.y }</p>
         : <p>loading initial coordinates</p>}
     </div>
   );
